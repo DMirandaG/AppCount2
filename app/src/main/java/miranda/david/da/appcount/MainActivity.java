13ece7a49cbefea8df7b70fb858e0ffe4d3b25c3
David MG <david_smg_92@hotmail.com>
2018-01-31 19:40:04 +0000

    añadido botón de resta y test

--- app/src/main/java/miranda/david/da/appcount/MainActivity.java
+++ app/src/main/java/miranda/david/da/appcount/MainActivity.java
@@ -2,12 +2,53 @@ package miranda.david.da.appcount;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView pantalla;
+    private Button boton1;
+    private int contador;
+    private Button boton2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Arrancando mi App");
+
+        pantalla = findViewById(R.id.textView);
+        contador = 0;
+        pantalla.setText("" + contador);
+
+        boton1 = findViewById(R.id.button);
+        //boton1.setOnClickListener(new ButtonListener());
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d ("MainActivity", "Boton pulsado");
+                contador++;
+                pantalla.setText("" + contador);
+            }
+        });
+
+        boton2 = findViewById(R.id.boton2);
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d ("MainActivity", "Boton pulsado");
+                contador--;
+                pantalla.setText("" + contador);
+            }
+        });
+
+
     }
 }
